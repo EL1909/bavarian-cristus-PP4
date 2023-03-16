b3fbd6941aa75faa21726668bb638c138cdd762a
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-03-16 10:31:02 +0000

    Commenting post sections is working succesfully for members

--- blog/models.py
+++ blog/models.py
@@ -36,7 +36,7 @@ class Comment(models.Model):
 
     text = models.TextField()
     image_post = models.ForeignKey(ImagePost, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posted')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments_made')
     name = models.CharField(max_length=80)
     email = models.EmailField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
