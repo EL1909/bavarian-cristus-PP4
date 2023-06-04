98b66699e6f8d531f6670cb625e0ae11f887d988
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-06-04 14:38:03 +0000

    Adding form validation

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 from .models import ImagePost, Comment
 
 
@@ -7,6 +8,17 @@ class ImagePostForm(ModelForm):
         model = ImagePost
         fields = ['title', 'image', 'location', 'text', 'status']
 
+    def validText(self):
+        cleaned_data = super().clean()
+        text = cleaned_data.get('text')
+        if text and len(text) < 6:
+            raise ValidationError("Comment must be at least 6 characters")
+
+    def validLocation(self):
+        location = self.cleaned_data['location']
+        if 'bayern' not in location.lower():
+            raise forms.ValidationError("Location must be within the state of Bavaria (Bayern).")
+        return location
 
 class CommentForm(ModelForm):
     class Meta:
